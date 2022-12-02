d623340625c48bfd426c26e5d0d29e56dde7c200
beckyweeks14 <mauransnow@Weeks-MacBook-Pro.local>
2022-12-02 15:13:05 -0500

    worked out card flip

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
 console.log("connected!")//delete later
 
-document.querySelector("div")
-.addEventListener("click", handleClick);
+const cardBack = "https://iili.io/Hf4LFi7.png"
+const pictures = [
+    "https://iili.io/HqziLFt.png", 
+    "https://iili.io/HqziPMN.png",
+    "https://iili.io/Hqzi6np.png", 
+    "https://iili.io/HqzirtR.png", 
+    "https://iili.io/HfspO8l.png", 
+    "https://iili.io/HqziQcX.png",
+    "https://iili.io/HqziLFt.png", 
+    "https://iili.io/HqziPMN.png",
+    "https://iili.io/Hqzi6np.png", 
+    "https://iili.io/HqzirtR.png", 
+    "https://iili.io/HfspO8l.png", 
+    "https://iili.io/HqziQcX.png",
+]
 
-function handleClick (evt) {
-    console.log(evt)
-}
+
+const parentEl = document.querySelector(".parent")
+parentEl.addEventListener('click', function(evt) {
+    console.log(evt.target.id)
+    console.log(pictures[evt.target.id])
+    evt.target.setAttribute("src", `${pictures[evt.target.id]}`)
+})
+
+// $('.parent div img').on('click', function (evt){
+//     console.log(evt.target.id)
+//     // evt.target.currentSrc = "https://iili.io/HqziLFt.png"
+// });
+
 
 // const status = 
-
-// const #card1 = $card1;
+    // const #card1 = $card1;
 // const .back = $imgBack
 // $(document).ready(function(){
 //     $imgBack.click(function(){
