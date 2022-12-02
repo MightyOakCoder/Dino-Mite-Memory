e4af07ef8b23007a4b7c65791eef7af508a7c265
beckyweeks14 <mauransnow@Weeks-MacBook-Pro.local>
2022-12-02 13:49:06 -0500

    updating today's progress

--- js/script.js
+++ js/script.js
@@ -8,3 +8,21 @@ function handleClick (evt) {
 }
 
 // const status = 
+
+// const #card1 = $card1;
+// const .back = $imgBack
+// $(document).ready(function(){
+//     $imgBack.click(function(){
+//       $card1.toggle();
+//     });
+//   });
+  
+// $card1.fadeOut(function() {
+//   trigger: 'click'
+// });
+
+// $(document).ready(function(){
+//   $img.click(function(){
+//     $card1.toggle();
+//   });
+// });
