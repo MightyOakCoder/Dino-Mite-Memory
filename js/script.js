6da2c67adfa538489fe980c243e109c78f071f62
beckyweeks14 <mauransnow@Weeks-MacBook-Pro.local>
2022-12-03 17:11:51 -0500

    Made additional updates

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 console.log("connected!")//delete later
 
+/*----- constants -----*/
+// const lookup = {
+//     "1": "Player 1",
+//     "-1": "Player 2"
+//     "null": "Null"
+// };
 const cardBack = "https://iili.io/Hf4LFi7.png"
 const pictures = [
     "https://iili.io/HqziLFt.png", 
@@ -16,21 +22,49 @@ const pictures = [
     "https://iili.io/HqziQcX.png",
 ]
 
-
 const parentEl = document.querySelector(".parent")
-const timesClicked = 0
 parentEl.addEventListener('click', function(evt) {
     console.log(evt.target.id)
     console.log(pictures[evt.target.id])
     evt.target.setAttribute("src", `${pictures[evt.target.id]}`)
 })
 
-function stopCick() {
-    timesClicked++;
-    if (timesClicked > 2) {
-        parentEl.removeEventListener("click", stopClick)
-    } else //continue to allow clicks
-};
+/*----- app's state (variables) -----*/
+ 
+  /*----- cached element references -----*/
+
+  /*----- event listeners -----*/
+  
+  /*----- functions -----*/
+
+
+// const clicks = 0
+
+// function onClick() {
+//     clicks += 1;
+//     document.getElementById("clicks").pictures = clicks;
+// }
+// console.log(clicks)
+
+
+// var button = pictures,
+//   count = 0;
+// button.onclick = function() {
+//   count += 1;
+//   button.innerHTML = "Click me: " + count;
+// };
+// console.log(count)
+
+
+
+// const timesClicked = 0
+// function stopClick() {
+//     timesClicked++;
+//     if (timesClicked > 2) {
+//         parentEl.removeEventListener("click", stopClick)
+//     } else //continue to allow clicks
+// };
+
 // function flipCard(cardChosen) {
 //     if (cardChosen != pictures[0] + pictures[6])
 //     setAttribute
@@ -66,15 +100,26 @@ function stopCick() {
 // });
 
 
+//What is said on screen when game is over
+// function render() {
+//     // board.forEach(function(sq, idx) {
+//     //     squares[idx].style.background = lookup[sq];
+//     //   });
+//       if (winner === 'T') {
+//       message.innerHTML = "It's a tie!";
+//     } else if (winner) {
+//       message.innerHTML = `Congrats ${lookup[winner].toUpperCase()}!`;
+//     } else {
+//       message.innerHTML = `Congrats ${lookup[turn].toUpperCase()}!`;
+//     }
+//   }
 
-/*----- constants -----*/
+//randomize the images
+window.onload = choosePic;
 
-/*----- app's state (variables) -----*/
- 
-  /*----- cached element references -----*/
+var randomPics = pictures
 
-  /*----- event listeners -----*/
-  
-  /*----- functions -----*/
-  
- 
+function choosePic() {
+    var randomNum = Math.floor(Math.random() * randomPics.length);
+    document.getElementsById("main").src = randomPics(randomNum);
+}
